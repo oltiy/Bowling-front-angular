5ef90f36d2b4f878a000810e2bb7ed9e7b34321f
oltiy <oltiyoni@gmail.com>
2022-05-11 12:40:51 +0300

    some updates

--- src/app/features/scores/scores.module.ts
+++ src/app/features/scores/scores.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { RouterModule, Routes } from '@angular/router';
-import { DomainModule } from 'src/app/domain/domain.module';
 
 import { ScoresComponent } from './scores.component';
 
@@ -10,11 +9,6 @@ const routes: Routes = [{ path: '', component: ScoresComponent }];
 
 @NgModule({
   declarations: [ScoresComponent],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes),
-    MatTableModule,
-    DomainModule,
-  ],
+  imports: [CommonModule, RouterModule.forChild(routes), MatTableModule],
 })
 export class ScoresModule {}
